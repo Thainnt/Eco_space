a4c6968bf073b43e5eb85a935182c18be3e11f25
salimM-code <mustafasalim000@gmail.com>
2021-11-20 18:52:09 -0500

    implemented navBar

--- frontend/src/components/Pages/Navbar.jsx
+++ frontend/src/components/Pages/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route, Link } from "react-router-dom";
-import styles from "../../styles.css"
+import Cookies from 'js-cookie'
+import {useNavigate} from 'react-router-dom';
 
 
-
-const Nav = () => {
+const Nav = (props) => {
+  const navigate = useNavigate()
+  const { loggedInUserName } = props;
+  const handleClick = () => {
+    Cookies.set("username", "")
+    navigate("/")
+  }
   return (
     <nav class='navbar'>
-      <h2>Eco Space</h2>
+      
       <ul>
         <li>
           <Link to="/About">About Us</Link>
@@ -17,12 +23,21 @@ const Nav = () => {
         <li>
           <Link to="/Store">Store</Link>
         </li>
-        <li>
-          <Link to="/Login">Log In</Link>
-        </li>
-        <li>
-          <Link to="/Register">Register</Link>
-        </li>
+        {loggedInUserName.length > 0 ? (
+          <>
+            <li>{loggedInUserName}</li>
+            <button onClick={handleClick}>Log Out </button> 
+          </>
+        ):(
+          <>
+            <li>
+              <Link to="/Login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/Register">Register</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
