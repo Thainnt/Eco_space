601a609558d2f62ed6bd121475fd5510e28b610e
salimM-code <mustafasalim000@gmail.com>
2021-11-20 18:56:21 -0500

    updated users endpoints

--- backend/db/user-queries.js
+++ backend/db/user-queries.js
@@ -6,9 +6,9 @@ const getAllUsers = () => {
 const addUser = (user) => {
   return db
     .query(
-      `INSERT INTO users(name, email, password)
-                    VALUES($1,$2,$3) RETURNING *;`,
-      [`${user.name}`, `${user.email}`, `${user.password}`]
+      `INSERT INTO users(name, email, password, is_admin)
+                    VALUES($1,$2,$3,$4) RETURNING *;`,
+      [`${user.name}`, `${user.email}`, `${user.password}`, `${user.is_admin}`]
     )
     .then((response) => {
       if (response.rows.length) {
