b8c6bfa15ca8b30747a162b1bdeab4b2d6280417
salimM-code <mustafasalim000@gmail.com>
2021-11-20 21:58:32 -0500

    backend routes for free products

--- backend/routes/storeP.js
+++ backend/routes/storeP.js
@@ -9,7 +9,7 @@ router.get("/products", (req, res) => {
   storePQueries.getAllStoreProduct().then((response) => {
     const products = response.rows;
     console.log(products);
-    res.json(products);
+    res.send({ products: products });
   });
 });
 
@@ -20,17 +20,58 @@ router.get("products/:id", (req, res) => {
   storePQueries.getSingleStoreProduct(prod_id).then((response) => {
     console.log(response);
     if (response.rows.length === 0) {
-      return res.status(404).send({ status: 'error', message: `Product not found with the ${prod_id}`})
+      return res
+        .status(404)
+        .send({
+          status: "error",
+          message: `Product not found with the ${prod_id}`,
+        });
     }
-    res.send({ product: response.rows[0] })
+    res.send({ product: response.rows[0] });
   });
 });
 //products (POST, creat new post)
-router.post("/new", (req, res) => {});
+router.post("/new", (req, res) => {
+  const Pname = req.body.name;
+  const quantity = req.body.quantity;
+  const description = req.body.description;
+  const image_url = req.body.image_url;
+  // const seller_id = req.
+  // const category_id = req.
+  const is_sold = false;
+  let is_paid = false;
+  let amount = req.body.amount;
+  let product = {
+    Pname,
+    quantity,
+    description,
+    image_url,
+    // seller_id,
+    // category_id,
+    is_sold,
+    is_paid,
+    amount,
+};
+
+  if (req.body.is_paid) {
+    is_paid = req.body.is_paid;
+    amount = req.body.amount;
+  }
+
+  productQueries.addProduct(product).then((response) => {
+    const newProduct = response.rows[0];
+
+    res.send({ ...newProduct });
+  })
+  .catch((error) => {
+    console.log("failed to added user", error)
+    res.status(400).send("can not add user")
+  })
+});
+
+
 //products/:id/edit -> frontend
 //products/:id (PUT/PATCH) -> UPDATING A SINGLE POST
 //products/:id (DELETE)  -> DELETE A SINGLE POST
-
-
 
 module.exports = router;
