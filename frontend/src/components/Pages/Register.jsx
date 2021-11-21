8c053e4c572d81cb487dbb465caa93d91b6d2ca2
faridAbinah <faridabinah@hotmail.com>
2021-11-20 19:14:05 -0500

    test

--- frontend/src/components/Pages/Register.jsx
+++ frontend/src/components/Pages/Register.jsx
@@ -1,33 +1,69 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Register(props) {
-  
+  const [name, setName ] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   function validate() {
 
   }
 
-  function submit(event) {
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  }
+   
+      axios.put('http://localhost:8080/api/users',{
+      name:name,
+      email:email,
+      password:password
+    }).then(res => {
+      console.log(res.data);
+      navigate('/');
+    }).catch(err => {
+      console.log(err);
+    })
+    }
+    
+  
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <label>
         Name:
-        <input type="text"  name="name" />
+        <input 
+         type="text"
+         requried 
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+         />
         </label>
 
         <label>
         Email:
-        <input type="text"  name="email" />
+        <input 
+         type="email" 
+         value={email}
+         requried 
+         onChange={(e) => setEmail(e.target.value)}
+         />
         </label>
 
         <label>
         Password: 
-        <input type="text"  name="password" />
+        <input
+         type="password"
+         requried 
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         />
         </label>
-
+        <button>Register</button>
     
        
 
