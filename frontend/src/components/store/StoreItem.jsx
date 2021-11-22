e24656c6dab1bc2fc370526da2bfa941a42edb00
salimM-code <mustafasalim000@gmail.com>
2021-11-21 19:12:12 -0500

    store page fixes

---
+++ frontend/src/components/store/StoreItem.jsx
@@ -0,0 +1,10 @@
+const StoreItem = (props) => {
+  const { name, quantity, description, image_url, seller_id, amount } = props
+  return ( 
+    <div className="item_container">
+
+    </div>
+   );
+}
+ 
+export default StoreItem;
--- frontend/src/components/store/store.jsx
+++ frontend/src/components/store/store.jsx
@@ -1,16 +1,16 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import useFetch from "../useFetch";
 
 const Store = () => {
-  // const url = "http://localhost:8080/api/store/products";
-  // const { data: products, isPending, error } = useFetch(url);
+  const [ products, setProducts] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:8080/api/store/products")
       .then(response => {
-        console.log(response.data.products)
+        setProducts(response.data.products)
       })
-  })
+  }, [])
+  
   return ( 
     // console.log(products)
     <>
