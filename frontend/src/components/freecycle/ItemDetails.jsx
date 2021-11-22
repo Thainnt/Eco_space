e18053ea47731c045c5184125f623f047f7d4139
Thainnt <thai164@gmail.com>
2021-11-22 22:23:21 +0000

    Add Item details page with back end logic

--- frontend/src/components/freecycle/ItemDetails.jsx
+++ frontend/src/components/freecycle/ItemDetails.jsx
@@ -1,20 +1,31 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import { useParams } from "react-router";
 
 
 export default function ItemDetails() {
+
   const { id } = useParams();
-  console.log(id)
+
+  const [item, setItem] = useState({});
 
   useEffect(() => {
     axios.get("/api/freecycle/products/" + id)
       .then(response => {
-        console.log("+++++===>", response);
+        setItem(response.data.product);
+      }).catch(error => {
+        console.error(error);
       })
   }, [id])
 
   return ( 
-    <p>hello</p>
+    <section className="item-details">
+      <h2>{item.name}</h2>
+      <img className="item-details-image" src={item.image_url} alt={item.name}/>
+      <p className="item-details-desc">{item.description}</p>
+      <span className="item-details-loc">Location: {item.location}</span>
+      <br/>
+      <button className="item-details-contact">Contact owner</button>
+    </section>
    );
 }
