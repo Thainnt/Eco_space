ae6f21ccb0229a782f6417cbf7de6338c047b6d1
salimM-code <mustafasalim000@gmail.com>
2021-11-23 14:16:51 -0500

    added socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 const storePRouter = require("./routes/storeP");
 const usersRouter = require("./routes/users");
 const freePRouter = require("./routes/freeP");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 8080;
 
 //middleware
@@ -30,6 +35,11 @@ app.use("/api/users", usersRouter);
 app.use("/api/store", storePRouter);
 app.use("/api/freecycle", freePRouter);
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log("a user connected", socket.id);
+})
+
+
+server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
