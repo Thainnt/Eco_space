cce4bdc1530f513bc46b2cb74b8b88a2fc6fdd8d
salimM-code <mustafasalim000@gmail.com>
2021-11-22 21:10:12 -0500

    navbar refactor

--- frontend/src/components/Pages/Navbar.jsx
+++ frontend/src/components/Pages/Navbar.jsx
@@ -2,8 +2,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import {useNavigate} from 'react-router-dom';
 import styles from "../../styles.css"
 import axios from "axios";
-import { IoIosPerson } from 'react-icons/io'
 import { StyledIcon } from "../styles/Icon";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge } from "@mui/material";
+import { Button } from "@mui/material";
+import { Container, Wrapper, Logo, Left, Right, MenuItem } from "../styles/nav.styled";
+
 
 
 const Nav = (props) => {
@@ -18,35 +22,40 @@ const Nav = (props) => {
       })
   }
   return (
-    <nav class='navbar'>
-      <h2>Eco Space</h2>
-      
-      <ul>
-        <li>
-          <Link to="/About">About Us</Link>
-        </li>
-        <li>
-          <Link to="/FreeCycle">Free Goods</Link>
-        </li>
-        <li>
-          <Link to="/store">Store</Link>
-        </li>
+    <Container class='navbar'>
+      <Wrapper>
+        <Left>
+          <Logo>Eco Space</Logo>
+        </Left>
+        <Right>
+          <Link to="/About">
+            <MenuItem>ABOUT US</MenuItem>
+          </Link>
+          <Link to="/cart">
+          <MenuItem>
+            <Badge color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Link>
         {loggedInUserName.length > 0 ? (
           <>
-            <li>{loggedInUserName}</li>
-            <button onClick={handleClick}>Log Out </button> 
+            <MenuItem>{loggedInUserName}</MenuItem>
+            <MenuItem>
+              <Button color="primary" onClick={handleClick}>LOG OUT</Button>
+            </MenuItem>
           </>
         ):(
-            <li>
-              <Link to="/Login"><StyledIcon size={25} /></Link>
-            </li>
+          <Link to="/Login">
+            <MenuItem>
+            <StyledIcon size={25} />
+            </MenuItem>
+          </Link>
         )}
-      <Routes>
-        <Route exact path="/store" element={<li> CART </li>} />
-      </Routes>
         
-      </ul>
-    </nav>
+        </Right> 
+      </Wrapper>
+    </Container>
   )
 }
 
