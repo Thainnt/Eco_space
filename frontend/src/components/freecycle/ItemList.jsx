281b8864c43c25aba7816588362b20679ddd783d
faridAbinah <faridabinah@hotmail.com>
2021-11-23 21:49:44 -0500

    Refactored freecycle page to resize items and change styles

--- frontend/src/components/freecycle/ItemList.jsx
+++ frontend/src/components/freecycle/ItemList.jsx
@@ -1,27 +1,54 @@
 import{ Link } from "react-router-dom";
+import { Container } from "../styles/Container.styled";
+
+
 
 export default function ItemList(props) {
 
   const { items, title } = props;
-  
+  const myStyles = {
+    width:"400px",
+    height:"450px"
+  }
   return (
-    <div className="item-list">
-      <h2>Category</h2>
-      {items.map((item) => (
-        <div className="item-preview" key={item.id}>
-          <Link to={`/freecycle/items/${item.id}`}>
-            <div className="item-card">
-              <div className="item-image-container">
-                <img className="item-image" src={item.image_url} alt={item.name}/>
-              </div>
+    <>
+    <h2>Category</h2>
+    <Container>
+      {items.map((item)  => (
+        <div className="product-preview" key={item.id}>
+          <Link to={`/freecycle/items/${item.id}`} >
+            <div className="item_card">
+              <img style={myStyles} className="item_image" src={item.image_url} alt={item.name}/>
               <span className="item_content">
-                <span className="item_name">{item.name}</span>
-                <p>{item.description}</p>
-              </span>
-            </div>
+                <span className="item_name">
+                   {item.name}
+                </span>
+                   
+                </span>
+                </div>
           </Link>
         </div>
-      ))}
-    </div>
+      ) )}
+    </Container>
+    </>
+
+    // <div className="item-list">
+    //   <h2>Category</h2>
+    //   {items.map((item) => (
+    //     <div className="item-preview" key={item.id}>
+    //       <Link to={`/freecycle/items/${item.id}`}>
+    //         <div className="item-card">
+    //           <div className="item-image-container">
+    //             <img className="item-image" src={item.image_url} alt={item.name}/>
+    //           </div>
+    //           <span className="item_content">
+    //             <span className="item_name">{item.name}</span>
+    //             <p>{item.description}</p>
+    //           </span>
+    //         </div>
+    //       </Link>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
