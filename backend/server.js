1c81f934c75b7228634c6ff086463861d89cf378
salimM-code <mustafasalim000@gmail.com>
2021-11-25 12:55:58 -0500

    stripe backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const chat = require('./chat');
-const socketio = require('socket.io');
+const chat = require("./chat");
+const socketio = require("socket.io");
 const express = require("express");
 const http = require("http");
 // const { Server } = require("socket.io");
@@ -12,14 +12,16 @@ const storePRouter = require("./routes/storeP");
 const usersRouter = require("./routes/users");
 const freePRouter = require("./routes/freeP");
 const contactRouter = require("./routes/contact");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server,{
+const io = socketio(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 chat(io);
 
@@ -37,17 +39,39 @@ app.use(
     keys: ["secrect key are really hard to break", "2565evchsb 66edvc3tf27"],
   })
 );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // routes
 app.use("/api/users", usersRouter);
 app.use("/api/store", storePRouter);
 app.use("/api/freecycle", freePRouter);
-app.use("/api/contact", contactRouter );
+app.use("/api/contact", contactRouter);
 
-// io.on('connection', (socket) => {
-//   console.log("a user connected", socket.id);
-// })
-
+//stripe setup
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "CAD",
+      description: "Eco Space",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("payment", payment);
+    res.json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
